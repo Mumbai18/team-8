a5548c6e5200bf33535c15e5543bd81d80e9ac03
Unknown <divitkarmiani1998@gmail.com>
2018-07-21 22:49:46 +0530

    php backend

--- WebApp/Website/login.php
+++ WebApp/Website/login.php
@@ -1,12 +1,14 @@
+<?php
+?>
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Annadhan</title>
+	<title>Login Page</title>
 	<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
 	<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
 	<link href="http://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css">
 
-	<link rel="stylesheet" type="text/css" href="/team-8/WebApp/Website/CSS/app.css">
+	<link rel="stylesheet" type="text/css" href="/team-8/WebApp/Website/CSS/login.css">
 </head>
 <body>
 
@@ -29,34 +31,45 @@
       </ul>
       <ul class="nav navbar-nav navbar-right">
         <li><a href="signup">Signup  <i class="fa fa-user-plus"></i></a></li>
-        <li><a href="/team-8/WebApp/Website/login.php">Login  <i class="fa fa-user"></i></a></li>
+        <li><a href="login">Login  <i class="fa fa-user"></i></a></li>
 
-
-<!-- <button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">Login<span class="caret"></span></button>
-	<ul class="dropdown-menu">
-		<li><a href="#">HTML</a></li>
-		<li><a href="#">CSS</a></li>
-		<li><a href="#">JavaScript</a></li>
-	</ul> -->
 
       </ul>
     </div>
   </div>
  </nav>
 
- <div class="container">
+<div class="container" id="loginform">
+  <form method="post" action="/team-8/WebApp/Website/authenticate.php">
+    <div class="form-group">
+      <label for="user_name">Username:</label>
+      <input type="text" class="form-control" id="user_name" placeholder="Enter username" name="user_name">
+    </div>
+    <div class="form-group">
+      <label for="password">Password:</label>
+      <input type="password" class="form-control" id="password" placeholder="Enter password" name="password">
+    </div>
+    <div class="checkbox">
+      <label><input type="checkbox" name="remember"> Remember me</label>
+    </div>
+    <button type="submit" class="btn btn-default">Submit</button>
+  </form>
+</div>
+
+
+<!--  <div class="container">
  	<div class="row">
  		<div class="col-lg-12">
  			<div class="content">
  				<h1>Annadhan</h1>
  				<h3>What's left is not over</h3>
  				<hr>
- 				<!-- <button class="btn btn-default btn-lg"><i class="fa fa-paw fa-fw"></i> Get Started!</button> -->
+ 				<button class="btn btn-default btn-lg"><i class="fa fa-paw fa-fw"></i> Get Started!</button>
  			</div>
  		</div>
  	</div>
  </div>
-
+ -->
  <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.4.js"></script>
  <script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
 
